cdcfdccc9aba491d5ee58fbb730f1dedfeeab1ca
Mason Murphy <murphyma@hendrix.edu>
2024-09-23 22:18:55 -0500

    Created Entry class, imported pub camera

---
+++ snapshot_journal/lib/Object/Entry.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+class Sheep{
+
+  Sheep({
+    required this.discription,
+    required this.date,
+    required this.picture,
+    }
+  );
+
+String discription;
+
+String date;
+
+Image picture;
+
+
+}
+
+
