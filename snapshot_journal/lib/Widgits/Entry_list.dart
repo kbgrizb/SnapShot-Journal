df6827c113013e79b15c926ca499a03dbe220ffc
Mason Murphy <murphyma@hendrix.edu>
2024-09-24 14:54:50 -0500

    Added // to Entry_list

--- snapshot_journal/lib/Widgits/Entry_list.dart
+++ snapshot_journal/lib/Widgits/Entry_list.dart
@@ -19,6 +19,7 @@ final Entry entry;
 
 Widget build(BuildContext context) {
   return ListTile(
+    // Need to implemnt the ability to pull an new image
     leading: Image.network(
       "https://ichef.bbci.co.uk/news/1536/cpsprodpb/89e1/live/5b736ea0-f0e6-11ee-aa67-b5d719b6a777.jpg.webp",
       height: 100,
