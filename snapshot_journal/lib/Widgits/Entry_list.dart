c7049563bffd3baa2e6af9896d641dee3c54696f
Mason Murphy <murphyma@hendrix.edu>
2024-09-25 22:53:28 -0500

    Edited Entry_list image display

--- snapshot_journal/lib/Widgits/Entry_list.dart
+++ snapshot_journal/lib/Widgits/Entry_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:snapshot_journal/Object/Entry.dart';
+import 'dart:io';
 
 typedef entryRemoved = Function(Entry entry, bool remove);
 
@@ -25,11 +26,14 @@ final DateTime now = DateTime.now();
 Widget build(BuildContext context) {
   return ListTile(
     // Need to implemnt the ability to pull an new image
-    leading: Image.network(
-      "https://ichef.bbci.co.uk/news/1536/cpsprodpb/89e1/live/5b736ea0-f0e6-11ee-aa67-b5d719b6a777.jpg.webp",
-      height: 200,
-      width: 150,
-      ),
+    leading: entry.imagePath != null
+          ? Image.file(
+              File(entry.imagePath!),
+              height: 100,
+              width: 100,
+              fit: BoxFit.cover,
+            )
+          : const Icon(Icons.image, size: 100),
       title: Column(children: [Row(children: [Text(now.toString().split(' ')[0])]), // updated the dates position
       TextField(maxLength: 100, decoration: 
       InputDecoration(hintText: "Write something . . .", hintStyle: TextStyle(color: Colors.black.withOpacity(.5))),)]),
