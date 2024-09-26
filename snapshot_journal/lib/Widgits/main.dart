fe0c8a175c418d44d236816fdb9d64f688cd2d6c
Mason Murphy <murphyma@hendrix.edu>
2024-09-25 22:40:19 -0500

    Added pages/camera to branch

--- snapshot_journal/lib/Widgits/main.dart
+++ snapshot_journal/lib/Widgits/main.dart
@@ -1,54 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:snapshot_journal/Object/Entry.dart';
 import 'package:snapshot_journal/Widgits/Entry_list.dart';
+import 'package:snapshot_journal/pages/journal_entries.dart';
 
-void main() {
-  runApp(MaterialApp(title: "Journal",
-  home: const JournalList(), 
-  theme: ThemeData(primarySwatch: Colors.deepPurple, scaffoldBackgroundColor: const Color.fromARGB(255, 218, 195, 161)),));
-}
+Future<void> main() async {
 
-// Changing StatelessWidget to StatefulWidget
-class JournalList extends StatefulWidget {
-  const JournalList({super.key});
-
-  @override
-  State<JournalList> createState() => _JournalListState();
-}
-
-class _JournalListState extends State<JournalList> {
-  final List<Entry> entries = [
-    Entry()
-  ];
+  WidgetsFlutterBinding.ensureInitialized();
 
 
-  @override
-  void initState() {
-    super.initState();
-  }
 
-  // Moving build method inside the _JournalListState class
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        padding: const EdgeInsets.symmetric(vertical: 15.0),
-        children: entries.map((entry) {
-          return EntryList(
-            entry: entry,
-          );
-        }).toList(),
+  runApp(
+    MaterialApp(
+      theme: ThemeData.dark(),
+      home: const JournalEntries(
+       
       ),
-    floatingActionButton: FloatingActionButton.extended(
-      label: const Text("Entry"),
-      icon: const Icon(Icons.add_a_photo_outlined),
-      onPressed: null)
-    );
-  }
+    ),
+  );
 }
-
-
-
-
-
-
---
+++ snapshot_journal/lib/pages/journal_entries.dart
@@ -0,0 +1,53 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:snapshot_journal/Object/Entry.dart';
+import 'package:snapshot_journal/Widgits/Entry_list.dart';
+
+class JournalEntries extends StatefulWidget {
+  const JournalEntries({
+    super.key,
+  });
+ @override
+  State<JournalEntries> createState() => _JournalEntriesState();
+}
+
+class _JournalEntriesState extends State<JournalEntries> {
+  final List<Entry> entries = [
+    Entry()
+  ];
+
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context)  {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome to SnapShot!'),
+        centerTitle: true,
+        bottom: const PreferredSize(
+            preferredSize: Size.zero,
+            child: Text(
+                "To use Snapshot, add a new picture then enter a text description.")),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 15.0),
+        children: entries.map((entry) {
+          return EntryList(
+            entry: entry,
+          );
+        }).toList(),
+      ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {}
+          
+    )
+    );
+  
+  }
+
+ 
+  }
